c2208fcdeafee532aecfa8c6e18689240905d9e9
Joshua Strutzel <jstrutzel01@csu.fullerton.edugit config user.name Joshua>
2025-04-18 01:31:28 -0700

    registered category and product models to admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Product
 
 # Register your models here.
+@admin.register(Category)
+class Category(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)} # Slug autofills as you type category name
+    list_display = ("name",) # In list view, show name column
+
+@admin.register(Product)
+class Product(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ("name",)
