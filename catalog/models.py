18e4e67c5abfe4e5b4e00a46724385bd85f4b84f
Joshua Strutzel <jstrutzel01@csu.fullerton.edugit config user.name Joshua>
2025-04-18 01:03:45 -0700

    Created Category model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50,unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ["name"] # Category objects will be sorted alphabetically when listed
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("product_detail", args=[self.id, self.slug]) #Returns the URL for a page that shows all products in this category
+
