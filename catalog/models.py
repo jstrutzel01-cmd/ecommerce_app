aa3fc3eb82169ef4474122ae1fec3fa7703ccdbc
Joshua Strutzel <jstrutzel01@csu.fullerton.edugit config user.name Joshua>
2025-04-18 01:17:00 -0700

    Created Product model

--- catalog/models.py
+++ catalog/models.py
@@ -14,3 +14,23 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse("product_detail", args=[self.id, self.slug]) #Returns the URL for a page that shows all products in this category
 
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="products")
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=110, unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    images = models.ImageField(upload_to="products/",blank=True)
+    created_at = models.DateField(auto_now_add=True) #Creates data field only when instance is created
+    updated_at = models.DateField(auto_now=True) #Updates the date field after save
+
+    class Meta:
+        ordering = ["name"]
+        indexes = [models.Index(fields=["id", "slug"])] #Creates a database index on id and slug for fast lookup
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("product_detail", args=[self.id, self.slug]) #Returns the URL for a page that shows all products in this category
