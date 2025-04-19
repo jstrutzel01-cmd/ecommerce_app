66c4fc889cdcb90096886c8a1715f0c40d7daf05
Joshua Strutzel <jstrutzel01@csu.fullerton.edugit config user.name Joshua>
2025-04-18 19:50:16 -0700

    fixed some syntax errors

--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator #Paginator breaks up lists into pages if too much too is being requested
 from .models import Product, Category
 
 def product_list(request, category_slug=None):
     category = None # This line will be overrided if slug is passed
+    products = Product.objects.all()
+    
     if (category_slug): # Function happens if category_slug is passed
         category = Category.objects.get(slug=category_slug) # Mathches category_slug to slug in DB
-        products = products.filter(category=category) # Filters products to include all products from the category
+        products = products.filter(category=category)
     
     paginator = Paginator(products, 8) #Creates paginator from product list to only show 8 per page
     page = request.GET.get("page", 1) #Get the current page number. Default to one
@@ -16,3 +18,7 @@ def product_list(request, category_slug=None):
                   {"products": products, "category": category},
                   )
 
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug)
+    return render(request, "catalog/product_detail.html", {"product" : product})
