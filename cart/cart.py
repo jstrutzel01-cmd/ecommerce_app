812f66f9a4b2eb68947da96efab85e9b2f0f4eab
Joshua Strutzel <jstrutzel01@csu.fullerton.edugit config user.name Joshua>
2025-04-19 01:41:50 -0700

    Added cart_detail template. Will add styling to all templates later

--- cart/cart.py
+++ cart/cart.py
@@ -10,15 +10,15 @@ class Cart:
     """
 
     def __init__(self, request):
-        self.session = request.session
-        cart = self.session.get("cart")
+        self.session = request.session # Accesses session data
+        cart = self.session.get("cart") # Gets "cart" from session if there is one from a previous session. "cart" is created from add
         if cart is None:
-            cart = self.session["cart"] = {}
+            cart = self.session["cart"] = {} # Crete empty cart if none
         self.cart = cart
 
     # --- core API ---
     def add(self, product, qty=1, override=False):
-        product_id = str(product.id)
+        product_id = str(product.id) # keys in session must be strings
         if product_id not in self.cart:
             self.cart[product_id] = {"qty": 0, "price": str(product.price)}
         if override:
@@ -39,13 +39,13 @@ class Cart:
 
     # --- helpers ---
     def __iter__(self):
-        product_ids = self.cart.keys()
-        products = Product.objects.filter(id__in=product_ids)
+        product_ids = self.cart.keys() # keys() returns a view of all the keys in that dictionary
+        products = Product.objects.filter(id__in=product_ids) #Use id__in to filter all ids in cart
         for p in products:
             item = self.cart[str(p.id)]
             item["product"] = p
             item["total_price"] = Decimal(item["price"]) * item["qty"]
-            yield item
+            yield item # Return one item at a time
 
     def __len__(self):
         return sum(item["qty"] for item in self.cart.values())
--- cart/views.py
+++ cart/views.py
@@ -4,9 +4,9 @@ from .cart import Cart
 
 def cart_add(request, product_id):
     cart = Cart(request)
-    product = get_object_or_404(Product, id=product_id, available=True)
+    product = get_object_or_404(Product, id=product_id)
     cart.add(product=product, qty=1)          # one click = +1
-    return redirect("cart:detail")
+    return redirect("cart:detail") # Jumps to cart_detail view
 
 def cart_remove(request, product_id):
     cart = Cart(request)
