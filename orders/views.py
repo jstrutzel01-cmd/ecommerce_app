19ff55a4a467f3a138257a128bd16c8678ba1a73
Joshua Strutzel <jstrutzel01@csu.fullerton.edugit config user.name Joshua>
2025-04-21 01:30:57 -0700

    Added templates for creating orders with input and order comfirmation

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from cart.cart import Cart
+from .models import OrderItem
+from .forms import OrderCreateForm
 
-# Create your views here.
+
+from django.shortcuts import render, redirect
+from cart.cart import Cart
+from .models import OrderItem
+from .forms import OrderCreateForm
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == "POST": # Checks if form was submitted
+        form = OrderCreateForm(request.POST) # Puts form data into form variable
+        if form.is_valid(): # Checks if input fields and were filled
+            order = form.save() # Now that form is valid store it into an order object
+            for item in cart: # From __iter__() in Cart class, Loop through each item in Cart
+                OrderItem.objects.create( 
+                    order    = order,
+                    product  = item["product"],
+                    price    = item["product"].price,
+                    quantity = item["qty"],
+                )
+            cart.clear()                              # empty cart
+            return render(request, "orders/created.html", {"order": order})
+    else:
+        form = OrderCreateForm()
+    return render(request, "orders/create.html", {"cart": cart, "form": form})
+
